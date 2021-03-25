76525e11677c55d22ddae3554743bd6fbcf95b13
shekhar <shekharpatle999@gmail.com>
2021-03-25 15:43:16 +0530

    Addded: compute a*b+c

--- ArithmaticComSort.sh
+++ ArithmaticComSort.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
-
-
-read b
-echo "Enter 3rd Input:"
-read c
+ read c
 
 #compute
 
 operation=`expr $a + $b \* $c`
+operation1=`expr $a + $b \* $c`
+operation2=`expr $a \* $b + $c`
 
 echo "Three inputs are :" $a $b $c
 echo "Operation:" $operation
+echo "Operation1:" $operation1
+echo "Operation2:" $operation2
