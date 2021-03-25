aa21ea9c13dcc4fa59a472949bb1928aef68331e
shekhar <shekharpatle999@gmail.com>
2021-03-25 15:38:33 +0530

    Addded: ComputeUC1

--- ArithmaticComSort.sh
+++ ArithmaticComSort.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
 
-echo "Enter 1st Input:"
-read a
-echo "Enter 2nd Input:"
 read b
 echo "Enter 3rd Input:"
 read c
 
+#compute
+
+operation=`expr $a + $b \* $c`
+
 echo "Three inputs are :" $a $b $c
+echo "Operation:" $operation
